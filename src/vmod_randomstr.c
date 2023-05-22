52a4a90c18a65c46ca190c9665d53d5e94ac661b
mech <wozniak.jacek@gmail.com>
2023-05-22 15:31:36 +0200

    Initialize the string pointer

--- src/vmod_randomstr.c
+++ src/vmod_randomstr.c
@@ -5,17 +5,22 @@
 #include <vcc_if.h>
 
 VCL_STRING
-vmod_randomstr(VRT_CTX, VCL_INT n, VCL_STRING s2)
+vmod_randomstr(VRT_CTX, VCL_INT n, VCL_STRING seed)
 {
     char *p;
     size_t len;
-    len = n;
     CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
+    CHECK_OBJ_NOTNULL(ctx->ws, WS_MAGIC);
+	if (seed == NULL)
+		return (NULL);
+
+    len = n;
 
     if (len >= WS_ReserveSize(ctx->ws, len + 1)) {
 		WS_Release(ctx->ws, 0);
 		return (NULL);
 	}
+    p = ctx->ws->f;
     while(n) {
         p[len - n] = 'a';
         n--;
